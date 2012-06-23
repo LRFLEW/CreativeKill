44760fccae1266d9302027759bb8080f1a0d953b
Lewis Fox <LRFLEW@aol.com>
2012-06-23 14:36:10 -0500

    Update 1.4
    Updated to Bukkit's new event system and added support for vehicles.

--- src/com/LRFLEW/bukkit/ck/CreativeKill.java
+++ src/com/LRFLEW/bukkit/ck/CreativeKill.java
@@ -1,47 +1,43 @@
 package com.LRFLEW.bukkit.ck;
 
 import net.minecraft.server.DamageSource;
+
 import org.bukkit.GameMode;
 import org.bukkit.craftbukkit.entity.CraftLivingEntity;
+import org.bukkit.craftbukkit.entity.CraftVehicle;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
-import org.bukkit.event.Event.Priority;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityListener;
-import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.event.vehicle.VehicleDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class CreativeKill extends JavaPlugin {
-	
-	public void onDisable() {
-		PluginDescriptionFile pdfFile = this.getDescription();
-		System.out.println( pdfFile.getName() + " says Goodbye!" );
-	}
+public class CreativeKill extends JavaPlugin implements Listener {
 	
 	public void onEnable() {
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvent(Event.Type.ENTITY_DAMAGE, new EntityEvents(), Priority.Monitor, this);
-		PluginDescriptionFile pdfFile = this.getDescription();
-		System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+		getServer().getPluginManager().registerEvents(this, this);
 	}
 	
-	private class EntityEvents extends EntityListener {
-		public void onEntityDamage(EntityDamageEvent e) {
-			if (e.isCancelled()) return;
-			if (!(e instanceof EntityDamageByEntityEvent)) return;
-			EntityDamageByEntityEvent event = (EntityDamageByEntityEvent)e;
-			if (!(event.getDamager() instanceof Player)) return;
-			Player player = (Player)event.getDamager();
-			if (!player.getGameMode().equals(GameMode.CREATIVE)) return;
-			if (!player.hasPermission("creativekill")) return;
-			if (!(event.getEntity() instanceof LivingEntity)) return;
-			LivingEntity le = (LivingEntity)event.getEntity();
-			if (le instanceof Player) return;
-			le.setHealth(0);
-			((CraftLivingEntity)le).getHandle().die(DamageSource.OUT_OF_WORLD);
-		}
+	@EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void entityDamageEntity(EntityDamageByEntityEvent e) {
+		if (!(e.getDamager() instanceof Player)) return;
+		Player player = (Player)e.getDamager();
+		if (!player.getGameMode().equals(GameMode.CREATIVE)) return;
+		if (!player.hasPermission("creativekill")) return;
+		LivingEntity le = (LivingEntity)e.getEntity();
+		if (le instanceof Player) return;
+		le.setHealth(0);
+		((CraftLivingEntity)le).getHandle().die(DamageSource.OUT_OF_WORLD);
+	}
+	
+	@EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void vehicleDamage(VehicleDamageEvent e) {
+		if (!(e.getAttacker() instanceof Player)) return;
+		Player player = (Player)e.getAttacker();
+		if (!player.getGameMode().equals(GameMode.CREATIVE)) return;
+		if (!player.hasPermission("creativekill")) return;
+		((CraftVehicle)e.getVehicle()).getHandle().die();
 	}
 }
